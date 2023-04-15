5b47f9611477601fec655527c4f0755b4cfc1c4a
Mark Vincze <mrk.vincze@gmail.com>
2023-04-15 18:51:12 +0200

    Add script for the sample site generation

---
+++ generate_sample_docs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+SOURCE_DIR=$PWD
+TEMP_REPO_DIR=$PWD/../sable-gh-pages
+
+echo "Removing temporary doc directory $TEMP_REPO_DIR"
+rm -rf $TEMP_REPO_DIR
+mkdir $TEMP_REPO_DIR
+
+echo "Cloning the repo with the gh-pages branch"
+git clone https://github.com/markvincze/sabledocs.git --branch gh-pages $TEMP_REPO_DIR
+
+pip install .
+
+cd sample
+sabledocs
+
+echo "Clear repo directory"
+cd $TEMP_REPO_DIR
+git rm -r *
+
+echo "Copy documentation into the repo"
+cp -r $SOURCE_DIR/sample/output/* .
+cp $SOURCE_DIR/README.md .
+
+echo "Push the new docs to the remote branch"
+git add . -A
+git commit -m "Update generated documentation"
+git push origin gh-pages
